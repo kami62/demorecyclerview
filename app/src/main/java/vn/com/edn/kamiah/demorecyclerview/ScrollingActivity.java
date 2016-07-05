378337f61a5fba8a88f3f876cfed26596b053a14
Le Trong Nghia <nghia.lt62@gmail.com>
2016-07-05 10:46:45 +0700

    update 1: - sua nut FAB

--- app/src/main/java/vn/com/edn/kamiah/demorecyclerview/ScrollingActivity.java
+++ app/src/main/java/vn/com/edn/kamiah/demorecyclerview/ScrollingActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -44,7 +45,12 @@ public class ScrollingActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                        .setAction("Action", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(ScrollingActivity.this, "I am Toast", Toast.LENGTH_SHORT).show();
+                            }
+                        }).show();
             }
         });
     }
